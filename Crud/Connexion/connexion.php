caceff48b1919abab5b2659427066fe3a7f4b3ff
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2024-12-24 16:29:38 +0100

    J'ai crée les variables contenent les information de mon database

---
+++ Crud/Connexion/connexion.php
@@ -0,0 +1,9 @@
+<?php
+    class Connexion{
+        // Inisialisez les variables contient les information pour accedez a mon database 
+        protected $host = 'localhost';
+        protected $user = 'root';
+        protected $password = '';
+        protected $database_name = 'contactify';
+    };
+?>
