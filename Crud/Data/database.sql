261c8c78ae13846ca4c5008d05bebb88270155ae
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2024-12-24 12:05:11 +0100

    J'ai Creez un table pour les contacts qui constitue de cind column le premier contient l'id qui est primary key est auto increment pour s'icrementez automatiquement et aussi il est de type int ensuit le column nom et prenom de type string et pour email et telephone sont aussi des strings

--- Crud/Data/database.sql
+++ Crud/Data/database.sql
@@ -3,3 +3,10 @@ CREATE DATABASE contactify;
 Use contactify;
 
 -- Create tables
+CREATE TABLE contacts(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    nom VARCHAR (255) NOT NULL,
+    prenom VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    telephone VARCHAR(255) NOT NULL
+);
